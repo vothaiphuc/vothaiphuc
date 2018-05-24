42d6377de26e53210a3c08a2589b4f3b1ef0d5ab
vothaiphuc <thaiphuc11cb1@gmail.com>
2018-05-25 00:30:10 +0700

    first commit

---
+++ source code/WebSiteBanSach/WebSiteBanSach/Controllers/NguoiDungController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanSach.Models;
+
+namespace WebSiteBanSach.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+
+        QuanLyBanSachEntities db = new QuanLyBanSachEntities();
+        // GET: /NguoiDung/
+        /*public ActionResult Index()
+        {
+            return View();
+        }*/
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+
+            return View();
+        }
+       /* [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public ActionResult DangKy(KhachHang kh)
+        {
+            if (ModelState.IsValid)
+            {
+                //Chèn dữ liệu vào bảng khách hàng
+                db.KhachHangs.Add(kh);
+                //Lưu vào csdl 
+                db.SaveChanges();
+            }
+            return View();
+        }*/
+        [HttpPost]
+        public ActionResult DangKy(FormCollection f,KhachHang kh)
+        {
+            //ViewBag.bao = "";
+            string hoten = f["txtHoTen"];
+            DateTime ngaysinh = DateTime.Parse(f["NgaySinh"]); 
+            string gioitinh = f["checkbox"];
+            string dienthoai = f["txtDienThoai"];
+            string tk = f["txtTaiKhoan"];
+            string mk = f["MatKhau"];
+            string xnmk = f["XacNhanMatKhau"];
+            string email = f["txtEmail"];
+            string diachi = f["txtDiaChi"];
+            if(tk==""||mk==""||xnmk==""||email==""||diachi==""||hoten==""||dienthoai=="")
+            {
+                ViewBag.bao = "Dữ liệu nhập vào không được rỗng";
+                return View();
+            }
+            //    else if(mk!=xnmk)
+            //{
+
+            //}
+            else
+            {
+                kh.HoTen = hoten;
+                kh.DienThoai = dienthoai;
+                kh.GioiTinh = gioitinh;
+                kh.TaiKhoan = tk;
+                kh.MatKhau = mk;
+                kh.Email = email;
+                kh.DiaChi = diachi;
+                kh.NgaySinh = ngaysinh;
+                db.KhachHangs.Add(kh);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+               
+            }
+            
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+
+            return View();
+        }
+        public ActionResult DangXuat()
+        {
+            Session["GioHang"] = null;
+            Session["TaiKhoan"] = null;
+            return RedirectToAction("DangNhap", "NguoiDung");
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            
+            string sTaiKhoan = f["txtTaiKhoan"].ToString();
+            string sMatKhau = f.Get("txtMatKhau").ToString();
+            KhachHang kh = db.KhachHangs.SingleOrDefault(n => n.TaiKhoan == sTaiKhoan && n.MatKhau == sMatKhau);
+            if(sTaiKhoan=="admin" && sMatKhau=="123456")
+            {
+                return RedirectToAction("Index", "QuanLySanPham");
+            }
+            if (sTaiKhoan == "" || sMatKhau == "")
+            {
+                ViewBag.bao = "Tài khoản và Mật khẩu không được để trống !";
+                return View();
+            }
+                
+            else if (kh == null)
+            {
+                ViewBag.ThongBao = "Tên tài khoản hoặc mật khẩu không đúng!";
+                return View();
+            }
+            else
+            {
+                if (Session["GioHang"] == null)
+                {
+                    Session["TaiKhoan"] = kh;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    Session["TaiKhoan"] = kh;
+                    return RedirectToAction("GioHang", "GioHang");
+                }
+                
+            }
+           
+            
+        }
+	}
+}
---
+++ source code/WebSiteBanSach/WebSiteBanSach/Models/Metadata/Sach.metadata.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using 2 thư viện thiết kế class metadata
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebSiteBanSach.Models
+{
+   [MetadataTypeAttribute(typeof(SachMetadata))]
+    public partial class Sach
+    {
+       internal sealed class SachMetadata
+       {
+
+           [Display(Name = "Mã sách")]//Thuộc tính Display dùng để đặt tên lại cho cột
+           public int MaSach { get; set; }
+           [Display(Name = "Tên sách")]
+           [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")] //Kiểm tra rổng
+           public string TenSach { get; set; }
+           [Display(Name = "Giá bán")]
+           public Nullable<decimal> GiaBan { get; set; }
+           [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")] 
+           [Display(Name = "Mô tả")]
+           public string MoTa { get; set; }
+           [DataType(DataType.Date)]
+           [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]//Định dạng ngày sinh
+           [Display(Name = "Ngày cập nhật")]
+           [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")] 
+           public Nullable<System.DateTime> NgayCapNhat { get; set; }
+           [Display(Name = "Ảnh bìa")]
+        
+           public string AnhBia { get; set; }
+           [Display(Name = "Số lượng tồn")]
+           [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")] 
+           public Nullable<int> SoLuongTon { get; set; }
+           [Display(Name = "Chủ đề")]
+          
+           public Nullable<int> MaChuDe { get; set; }
+           [Display(Name = "Nhà xuất bản")]
+     
+           public Nullable<int> MaNXB { get; set; }
+           [Display(Name = "Mới")]
+           [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")] 
+           public Nullable<int> Moi { get; set; }
+       }
+    }
+}
